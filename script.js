8067e70d782b958aff283f94a92beb28ae35f27b
elizianeandreia <liziandreia@gmail.com>
2025-01-27 09:50:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const CoresEscuras=[
-    #000000,
-    #00008B,
-    #778899,
-    #5F9EA0,
-    #2F4F4F,
-    #8B4513,
-    #4B0082,
-    #DC143C,
-
+const CoresEscuras = [
+    "#000000",
+    "#00008B",
+    "#778899",
+    "#5F9EA0",
+    "#2F4F4F",
+    "#8B4513",
+    "#4B0082",
+    "#DC143C",
 ];
 
-function obterIndiceIndex(){
- const indiceIndex =Math.floor(CoresEscuras.length * Math.random());
+function obterIndiceIndex() {
+ const indiceIndex = Math.floor(CoresEscuras.length * Math.random());
  return indiceIndex;
 }
 
 const body = document.querySelector("body");
-const bgCodigoHexSpanElement =document.querySelector("bg-codigo-hex");
+const bgCodigoHexSpanElement =document.querySelector("#bg-codigo-hex");
 
 function mudeCordeFundo(){
-const cor= CoresEscuras[obterIndiceIndex()];
+const cor = CoresEscuras[obterIndiceIndex()];
 bgCodigoHexSpanElement.innerText =cor;
-body.style.background =cor;
+body.style.backgroundColor =cor;
 }
 
-const btn;
+const btn = document.querySelector("#btn");
+btn.onclick= mudeCordeFundo();
 
